43eb440e082a96a179ef8df263a56808598a8c03
Julian Cook <juliantcook@gmail.com>
2015-11-02 22:23:07 +0000

    fresh spring-boot gradle app

---
+++ src/main/groovy/uk/julianc/thumblr/MainConfiguration.groovy
@@ -0,0 +1,12 @@
+package uk.julianc.thumblr
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class MainConfiguration {
+
+    static void main(String[] args) {
+        SpringApplication.run(MainConfiguration.class, args)
+    }
+}
